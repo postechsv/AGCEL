459341a702e8adfd0a3faa73943727cc91985e28
byhoson <byhoson@postech.ac.kr>
2024-11-26 00:14:43 +0900

    update train

--- train.py
+++ train.py
@@ -13,11 +13,32 @@ maude.init()
 maude.load(model)
 m = maude.getCurrentModule()
 
-env = MaudeEnv(m,prop,lambda : init,abst_mode='full')
+env = MaudeEnv(m,prop,lambda : init)
+
+### Train
 learner = QLearner()
 print(f'training {m}, {init} |= {prop} ... with {N} samples')
-stat = learner.train(env, N)
+learner.train(env, N)
 print('qtable size :', learner.get_size())
-learner.dump('qtable.maude', str(m))
-#learner.dump2('score.maude', m)
-print('dumped qtable : qtable.maude')
+#learner.dump(f'qtable-{prop}.maude', str(m), prop)
+learner.dump_value_function(f'value-function-{prop}.maude')
+
+
+#learner.dump2(f'score-{prop}.maude', m, prop)
+#print('dumped qtable : qtable.maude')
+
+
+### Search right after training ###
+from AGCEL.AStar import *
+
+init = m.parseTerm('init3')
+#init.reduce()
+n0 = Node(m, init)
+V = learner.get_value_function()
+#res = Search().search(n0, V, 9999)
+
+#if res[0]:
+#    print('n_states:', res[2])
+#    res[1].print_term()
+
+
