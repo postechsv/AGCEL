f5af69888c3e90496c542efdcd14f6178cc67ab2
byhoson <byhoson@postech.ac.kr>
2024-12-23 17:53:52 +0900

    examples/filter.py : add testcases for MaudeEnv

--- examples/filter.py
+++ examples/filter.py
@@ -14,6 +14,37 @@ maude.init()
 maude.load(model)
 m = maude.getCurrentModule()
 
+"""
+Test for MaudeEnv
+"""
+
+# Test : MaudeEnv.__init__
+print(type(m))
 env = MaudeEnv(m,prop,lambda : init)
 
+# Test : MaudeEnv.reset to init
+env.reset()
 
+# Test : MaudeEnv.get_obs
+obs = env.get_obs()
+print('G_state:', obs['G_state'])
+print('state:', obs['state'])
+print('actions:', obs['actions'])
+
+# Test : MaudeEnv.reset to a particular state
+env.reset(obs['G_state'])
+
+# Test : MaudeEnv.step
+env.step(obs['actions'][0])
+
+# Test : MaudeEnv.obs
+env.obs(obs['G_state'])
+
+# Test : MaudeEnv.obs_act
+[env.obs_act(label,sb) for label in env.rules for _, sb, _, _ in env.G_state.apply(label)]
+
+# Test : MaudeEnv.get_rew
+env.get_rew()
+
+# Test : MaudeEnv.is_done
+env.is_done()
