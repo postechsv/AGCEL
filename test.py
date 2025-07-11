a88f48bc16e05574b7214a5d8aafd73cc3992cd4
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-07-11 19:08:37 +0900

    feat: implement oracle trace-based Q-table pretraining

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import time
 import sys
 
 # Usage: python3 test.py <maude_model> <init_term> <goal_prop> <qtable_file>
-# python3 test.py testcases/filter-5.maude init twoCrits trained/filter-init3-twoCrits-500-o1.agcel
+# python3 test.py testcases/filter-4.maude init twoCrits trained/filter-init4-twoCrits-500-o1.agcel
 
 model = sys.argv[1]
 init = sys.argv[2]
@@ -41,8 +41,7 @@ if res0[0]:
     #res0[1].print_term()
 
 
-# ----- SEARCH WITH QTABLE -----
-print('\n=== SEARCH WITH QTABLE ===')
+# Load pretrained value function
 learner = QLearner()
 learner.load_value_function(qtable_file, m)
 V = learner.get_value_function()
@@ -54,26 +53,5 @@ end_time = time.perf_counter()
 print('[QTABLE] n_states:', res[2])
 print(f'[QTABLE] Elapsed time: {(end_time - start_time)*1000:.3f} ms')
 if res[0]:
-    print('[QTABLE] Goal reached!')
-    # res[1].print_term()
-
-
-# # ----- SEARCH WITH LOCAL REGRESSION -----
-# from AGCEL.LocalRegression import LocalRegressionScore
-
-# print('\n=== SEARCH WITH LOCAL REGRESSION ===')
-# k = 1
-# min_nbrs = 5
-# loc_reg = LocalRegressionScore(k, min_nbrs)
-# loc_reg.train(qtable_file)
-# V_loc = loc_reg.get_value_function()
-
-# start_time = time.perf_counter()
-# res_loc = Search().search(n0, V_loc, 9999)
-# end_time = time.perf_counter()
-
-# print(f'[LOCAL REG] LocalRegressionScore(k={k}, min_nbrs={min_nbrs})')
-# print('[LOCAL REG] n_states:', res_loc[2])
-# print(f'[LOCAL REG] Elapsed time: {(end_time - start_time)*1000:.3f} ms')
-# if res_loc[0]:
-#     print('[LOCAL REG] Goal reached!')
+    print('[TRAINED] Goal reached!')
+    res[1].print_term()
