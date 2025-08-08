bd321803ea034c371365a1948c95e6dbb3e85816
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-08 13:26:41 +0900

    chore: add description

--- AGCEL/QCompression.py
+++ AGCEL/QCompression.py
@@ -1,4 +1,5 @@
-def extract_predicate_vector():
+def extract_predicate_vector(obs_term):
+    """Extract predicate names and their truth values from obs(...)"""
     return
 
 def compress_qtable_pairwise():
