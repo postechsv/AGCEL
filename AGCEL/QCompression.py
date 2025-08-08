d6a424fb0631dfd38a61699c65476ff84f32d8fa
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-08 13:36:36 +0900

    feat: define flatten function

--- AGCEL/QCompression.py
+++ AGCEL/QCompression.py
@@ -2,7 +2,10 @@ def extract_predicate_vector(obs_term):
     """Extract predicate names and their truth values from obs(...)"""
 
     pred_container = obs_term.arguments()[0]
-    
+
+    def flatten(t):
+        ...
+
     return
 
 def compress_qtable_pairwise():
