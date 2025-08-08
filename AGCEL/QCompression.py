a139fa664dc05ab91f00b28c8caec1a0cc5a60b7
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-08 13:35:34 +0900

    feat: extract inner predicate container from obs

--- AGCEL/QCompression.py
+++ AGCEL/QCompression.py
@@ -1,5 +1,8 @@
 def extract_predicate_vector(obs_term):
     """Extract predicate names and their truth values from obs(...)"""
+
+    pred_container = obs_term.arguments()[0]
+    
     return
 
 def compress_qtable_pairwise():
