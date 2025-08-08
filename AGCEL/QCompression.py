5a758d74a8b9073af98e2e36c2b6bd5c70be9708
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-08 13:41:24 +0900

    feat: handle single predicate structures

--- AGCEL/QCompression.py
+++ AGCEL/QCompression.py
@@ -1,6 +1,7 @@
 def extract_predicate_vector(obs_term):
     """Extract predicate names and their truth values from obs(...)"""
 
+    preds = []
     pred_container = obs_term.arguments()[0]
 
     def flatten(t):
@@ -8,6 +9,10 @@ def extract_predicate_vector(obs_term):
         if sym in ('_;_', 'and'):
             for arg in t.arguments():
                 flatten(arg)
+        else:
+            pname = t.symbol().getName()
+            val = t.arguments()[0].symbol().getName().lower() == 'true'
+            preds.append((pname, val))
 
     return
 
