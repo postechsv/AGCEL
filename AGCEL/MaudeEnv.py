cd9c171e8dd25c6797074896ef96056b1b15d7e2
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-11 08:13:33 +0900

    refactor: move extract_predicate_vector to MaudeEnv.py

--- AGCEL/MaudeEnv.py
+++ AGCEL/MaudeEnv.py
@@ -64,3 +64,25 @@ class MaudeEnv():
             return True
         else:
             return False
+        
+    # Extract predicate names and their truth values from obs(...)
+    def extract_predicate_vector(obs_term):
+
+        preds = []
+        pred_container = list(obs_term.arguments())[0]
+
+        def flatten(t):
+            sym = str(t.symbol())
+            if sym in ('_;_', 'and', '_`,_'):
+                for arg in t.arguments():
+                    flatten(arg)
+            elif sym == '_:_' and len(list(t.arguments())) == 2:
+                pred_term = list(t.arguments())[0]
+                bool_term = list(t.arguments())[1]
+                pname = str(pred_term.symbol())
+                val = str(bool_term.symbol()).lower() == 'true'
+                preds.append((pname, val))
+
+        flatten(pred_container)
+        print(f'[LOG] Final predicate vector: {preds}')
+        return preds
