06cf2b6a57bcf917895501383810490bbaa86542
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-11 14:52:43 +0900

    chore: add description

--- AGCEL/PA2.py
+++ AGCEL/PA2.py
@@ -1,9 +1,9 @@
 class Pattern:
-    mask_known: int
-    mask_val: int
+    mask_known: int # 1: (0 or 1) fixed position / 0: 'T' (masked)
+    mask_val: int   # value bits of mask_known positions
 
-def popcount():
+def popcount(x: int):   # the number of 1-bits of x
     pass
 
-def bitmask():
+def bitmask(idxs):  # bitmask with idxs bits set to 1
     pass
