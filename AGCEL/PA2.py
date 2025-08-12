316a999c1f7e4c2f5c20b250a6b228c52b7a498a
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-12 11:09:49 +0900

    refactor: switch bit order(p1→MSB), rename functions

--- AGCEL/PA2.py
+++ AGCEL/PA2.py
@@ -4,20 +4,20 @@ class Pattern:
     mask: int   # 0: (0,1) fixed position / 1: (T) masked position
     val: int    # value bits at fixed(non-masked) positions
 
-def popcount(x: int):   # the number of 1-bits of x
+def popcnt(x: int):   # the number of 1-bits of x
     return x.bit_count()
 
-def bitmask(idxs):  # bitmask with idxs bits set to 1
+def maskpos(idxs, n: int):  # bitmask with idxs bits set to 1
     m = 0
     for i in idxs:
-        m |= (1 << i)
+        m |= (1 << (n - 1 - i))
     return m
 
-def matches(p: Pattern, b: int):  # check if bit vector matches the pattern
+def match(p: Pattern, b: int):  # check if bit vector matches the pattern
     return (b & ~p.mask) == (p.val & ~p.mask)
 
-def choose_positions(n: int, t: int):
+def combos(n: int, t: int):     # all index combinations of size t
     return list(itertools.combinations(range(n), t))
 
-def generalize_bits(b: int, n: int, t: int):    # pattern masking (t positions)
+def patterns(b: int, n: int, t: int):    # pattern masking (t positions)
     pass
