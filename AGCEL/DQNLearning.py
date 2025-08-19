65bac385dc9b09c4078fefe0e4f0fe4c72f02d19
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-19 17:47:49 +0900

    feat: initialize Q-networks and optimizer

--- AGCEL/DQNLearning.py
+++ AGCEL/DQNLearning.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+import torch.optim as optim
 
 class DQN(nn.Module):
     def __init__(self, input_dim, output_dim):
@@ -16,3 +18,10 @@ class DQNLearner():
                  gamma, lr, tau):
         self.env = env
         self.encoder = state_encoder
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.q_net      = DQN(input_dim, num_actions).to(self.device)
+        self.target_net = DQN(input_dim, num_actions).to(self.device)
+        self.target_net.load_state_dict(self.q_net.state_dict())
+        self.optimizer  = optim.Adam(self.q_net.parameters(), lr=lr)
