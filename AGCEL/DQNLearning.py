9d51ed26419683b3ddaeda0e4b78c4eaa1cd1e9c
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-19 16:25:04 +0900

    feat: define basic MLP for DQN

--- AGCEL/DQNLearning.py
+++ AGCEL/DQNLearning.py
@@ -3,7 +3,11 @@ import torch.nn as nn
 class DQN(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(DQN, self).__init__()
-        # TODO
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_dim)
+        )
     def forward(self, x):
         pass  # TODO
 
