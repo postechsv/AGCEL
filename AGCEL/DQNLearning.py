0f243a46bf85cd45cc918578cdb4d9b75565a33e
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-19 16:13:40 +0900

    init: create DQN class

---
+++ AGCEL/DQNLearning.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+
+class DQN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(DQN, self).__init__()
+        # TODO
+    def forward(self, x):
+        pass  # TODO
