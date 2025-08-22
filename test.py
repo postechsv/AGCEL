bd422970916ab287183176405195e01ec36fc3b8
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-08-22 12:51:31 +0900

    feat: add testing code

--- test.py
+++ test.py
@@ -2,11 +2,15 @@ import maude
 from AGCEL.MaudeEnv import MaudeEnv
 from AGCEL.QLearning import QLearner
 from AGCEL.AStar import *
+from AGCEL.AStar import *
+from AGCEL.DQNLearning import DQNLearner
+from AGCEL.common import make_encoder
+import json
 import time
 import sys
 
 # Usage: python3 test.py <maude_model> <init_term> <goal_prop> <qtable_file>
-# python3 test.py testcases/filter-4.maude init twoCrits trained/filter-init4-twoCrits-500-o1.agcel
+# python3 test.py testcases/filter-4.maude init twoCrits trained/filter-init4-twoCrits-500-o1
 
 model = sys.argv[1]
 init = sys.argv[2]
@@ -19,13 +23,13 @@ m = maude.getCurrentModule()
 
 env = MaudeEnv(m, prop, lambda: init)
 
-print('\n=== SEARCH WITHOUT TRAINING ===')
 init_term = m.parseTerm(init)
 init_term.reduce()
 n0 = Node(m, init_term)
 
 V0 = lambda node: 0
 
+print('\n=== SEARCH WITHOUT TRAINING ===')
 start_time = time.perf_counter()
 res0 = Search().search(n0, V0, 9999)
 end_time = time.perf_counter()
@@ -38,7 +42,7 @@ if res0[0]:
 
 # Load pretrained value function
 learner = QLearner()
-learner.load_value_function(qtable_file + ".agcel", m)
+learner.load_value_function(qtable_file + '.agcel', m)
 V = learner.get_value_function()
 
 print('\n=== SEARCH WITH TRAINED VALUE FUNCTION ===')
@@ -51,3 +55,25 @@ print(f'[TRAINED] Elapsed time: {(end_time - start_time)*1000:.3f} ms')
 if res[0]:
     print('[TRAINED] Goal reached!')
     res[1].print_term()
+
+
+dqn_model_file = qtable_file + '-dqn.pt'
+dqn_vocab_file = qtable_file + '-dqn-vocab.json'
+
+with open(dqn_vocab_file, 'r') as f:
+    vocab = json.load(f)
+dqn = DQNLearner(env, state_encoder=make_encoder(vocab), input_dim=len(vocab),
+                 num_actions=len(env.rules), gamma=0.95, lr=1e-3, tau=0.01)
+dqn.load_model(dqn_model_file)
+V_dqn = dqn.get_value_function()
+
+print('\n=== SEARCH WITH DQN VALUE FUNCTION ===')
+start_time = time.perf_counter()
+res_dqn = Search().search(n0, V_dqn, 9999)
+end_time = time.perf_counter()
+
+print('[DQN] n_states:', res_dqn[2])
+print(f'[DQN] Elapsed time: {(end_time - start_time)*1000:.3f} ms')
+if res_dqn[0]:          
+    print('[DQN] Goal reached!')
+    res_dqn[1].print_term()
