d20ff19f4e287871b8faafecb1eca7ed0c4d9d30
Hyeyoon Kang <hyoonk@postech.ac.kr>
2025-12-25 01:34:23 +0900

    fix: fix predicate append logic

--- AGCEL/common.py
+++ AGCEL/common.py
@@ -11,7 +11,7 @@ def extract_predicate_vector(obs_term): # [("critLHS", True), ("waitLHS", False)
             for a in t.arguments(): flatten(a)
         elif sym == '_:_' and len(list(t.arguments())) == 2:
             p, b = list(t.arguments())
-            
+            pred_name = p.prettyPrint(maude.PRINT_WITH_PARENS)
     flatten(pred_container)
     return preds
 
